1e7910df6b522c712f40dae9f35179fe0c40f23c
Quentin Lemoigne <21700899@etu.unicaen.fr>
2019-02-26 15:17:38 +0100

    Ajout routes contact, add

--- ember-td3/app/adapters/application.js
+++ ember-td3/app/adapters/application.js
@@ -1,4 +1,5 @@
-import DS from 'ember-data';
+import LSAdapter from 'ember-localstorage-adapter';
 
-export default DS.JSONAPIAdapter.extend({
+export default LSAdapter.extend({
+  namespace: 'contact-app'
 });
